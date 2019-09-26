a841b89786a6e4e414ff8adb5cb250f3515e123c
Carl Aiau <hello@carlaiau.com>
2019-09-27 11:05:33 +1200

    Goose additions and runs

--- processing/jsonl/index.sh
+++ processing/jsonl/index.sh
@@ -1,6 +1,6 @@
 
-for file in article/*
+for file in goose/*
 do
-    curl -s -H "Content-Type: application/x-ndjson" -XPOST "localhost:9200/article_bm25/_bulk" --data-binary "@$file"
+    curl -s -H "Content-Type: application/x-ndjson" -XPOST "localhost:9200/goose/_bulk" --data-binary "@$file"
     echo "@$file indexed!"
 done 
--- python-other-frameworks/runner.sh
+++ python-other-frameworks/runner.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-python3 main.py bte ~/School/data/ready/a.xml ~/School/data/further-bp/bte/a.xml &
-python3 main.py bte ~/School/data/ready/b.xml ~/School/data/further-bp/bte/b.xml &
-python3 main.py bte ~/School/data/ready/c.xml ~/School/data/further-bp/bte/c.xml &
-python3 main.py bte ~/School/data/ready/d.xml ~/School/data/further-bp/bte/d.xml &
-python3 main.py bte ~/School/data/ready/e.xml ~/School/data/further-bp/bte/e.xml &
-python3 main.py bte ~/School/data/ready/f.xml ~/School/data/further-bp/bte/f.xml &
-python3 main.py bte ~/School/data/ready/g.xml ~/School/data/further-bp/bte/g.xml &
-python3 main.py bte ~/School/data/ready/h.xml ~/School/data/further-bp/bte/h.xml &
-python3 main.py bte ~/School/data/ready/i.xml ~/School/data/further-bp/bte/i.xml & 
-# python3 main.py bte ~/School/data/ready/j.xml ~/School/data/further-bp/bte/j.xml
-# python3 main.py bte ~/School/data/ready/k.xml ~/School/data/further-bp/bte/k.xml
-# python3 main.py bte ~/School/data/ready/l.xml ~/School/data/further-bp/bte/l.xml
-# python3 main.py bte ~/School/data/ready/m.xml ~/School/data/further-bp/bte/m.xml
-# python3 main.py bte ~/School/data/ready/n.xml ~/School/data/further-bp/bte/n.xml
-# python3 main.py bte ~/School/data/ready/o.xml ~/School/data/further-bp/bte/o.xml
-# python3 main.py bte ~/School/data/ready/p.xml ~/School/data/further-bp/bte/p.xml
-# python3 main.py bte ~/School/data/ready/q.xml ~/School/data/further-bp/bte/q.xml
-# python3 main.py bte ~/School/data/ready/r.xml ~/School/data/further-bp/bte/r.xml
-# python3 main.py bte ~/School/data/ready/s.xml ~/School/data/further-bp/bte/s.xml
-# python3 main.py bte ~/School/data/ready/t.xml ~/School/data/further-bp/bte/t.xml
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/e.xml ~/School/data/further-bp/goose/e.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/f.xml ~/School/data/further-bp/goose/f.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/g.xml ~/School/data/further-bp/goose/g.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/h.xml ~/School/data/further-bp/goose/h.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/i.xml ~/School/data/further-bp/goose/i.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/j.xml ~/School/data/further-bp/goose/j.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/k.xml ~/School/data/further-bp/goose/k.out & 
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/l.xml ~/School/data/further-bp/goose/l.out &
+#python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/m.xml ~/School/data/further-bp/goose/m.out &
+# python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/n.xml ~/School/data/further-bp/goose/n.out &
+# python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/o.xml ~/School/data/further-bp/goose/o.out &
+# python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/p.xml ~/School/data/further-bp/goose/p.out &
+python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/q.xml ~/School/data/further-bp/goose/q.out
+# python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/r.xml ~/School/data/further-bp/goose/r.out &
+# python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/s.xml ~/School/data/further-bp/goose/s.out &
+# python ~/School/python-other-frameworks/main.py goose ~/School/data/ready/t.xml ~/School/data/further-bp/goose/t.out &
